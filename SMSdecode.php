e4f6312c9bdcfe38febc1e48838e286c53795886
ragnarensar <ragnarensar@vodafone.de>
2025-04-08 23:07:28 +0200

    cleaned up

--- SMSdecode.php
+++ SMSdecode.php
@@ -1,17 +1,12 @@
 #!/usr/bin/php
 <?php
-// Klasse "PduFactory" zum Decodieren von SMS-PDUs einbinden
+
 require_once('PduFactory/Pdu/PduFactory.php');
 require_once('PduFactory/Pdu/Pdu.php');
 require_once('PduFactory/Utf8/Utf8.php');
 require_once('PduFactory/Exception/InvalidArgumentException.php');
 
-// Erstelle neues Objekt der Klasse "PduFactory"
 $SMSdecode = NEW Application\Pdu\PduFactory();
-
-// '0791947107160000240C919461638398530000610180919063802046BF1C442DCFE97390581EAE8FD174503BEC0651CB733A394C2FBB5D'
-//print_r($SMSdecode->decode($argv[1]));
-//*
 
 foreach($SMSdecode->decode($argv[1]) as $key => $value)
 {
@@ -26,5 +21,5 @@ foreach($SMSdecode->decode($argv[1]) as $key => $value)
 		}
 	}
 }
-//*/
+
 ?>
--- SMSencode.php
+++ SMSencode.php
@@ -1,15 +1,12 @@
 #!/usr/bin/php
 <?php
-// Klasse "PduFactory" zum Codieren von SMS-PDUs einbinden
+
 require_once('PduFactory/Pdu/PduFactory.php');
 require_once('PduFactory/Pdu/Pdu.php');
 require_once('PduFactory/Utf8/Utf8.php');
 require_once('PduFactory/Exception/InvalidArgumentException.php');
 
-// Erstelle neues Objekt der Klasse "PduFactory"
 $SMSencode = NEW Application\Pdu\PduFactory();
-
-#/usr/local/smi/share/php/SMSencode.php --message test --receiver 4523 --alphabetSize 7
 
 $longopts  = array(
     "message:",			// 
@@ -22,20 +19,13 @@ $longopts  = array(
     "receipt:",			// true / false
 );
 
-# define array for hold the list of parameters to encode function
 $PDUelements=array();
 
-# fill the array with the parameters 
 foreach(getopt("", $longopts) as $key => $value) $PDUelements[$key] = $value;
-#echo print_r($PDUelements); // debug
 
-# define array for hold the return output of encode function
 $output=array();
 
-# call the encode function with the list of parameters array and cache the return output (array)
 $output=$SMSencode->encode($PDUelements);
-#echo print_r($output); // debug
 
-# print the return output of encode function
 echo $output['byte_size']." ".$output['message']."\n";
 ?>
